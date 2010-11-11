0dbce20ce753cf8376f93be71c1a156b765adbfe
Ludo <kontsek@tovek.cz>
2010-11-11 16:33:36 +0100

    INT_MAX header <limits.h> added

--- src/core/CLucene/search/spans/TermSpans.cpp
+++ src/core/CLucene/search/spans/TermSpans.cpp
@@ -4,6 +4,8 @@
  * Distributable under the terms of either the Apache License (Version 2.0) or 
  * the GNU Lesser General Public License, as specified in the COPYING file.
  ------------------------------------------------------------------------------*/
+#include <limits.h>
+
 #include "CLucene/_ApiHeader.h"
 #include "CLucene/index/Terms.h"
 #include "CLucene/index/Term.h"
