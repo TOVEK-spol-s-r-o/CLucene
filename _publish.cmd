ea5ea9aa9d6ee811740d4b3ab77020c570117f15
borekk <kostka@tovek.cz>
2012-01-31 10:04:21 +0100

    publish script copied zlib1.dll with unpatched manifest to release build

--- _publish.cmd
+++ _publish.cmd
@@ -1,8 +1,14 @@
 %~d0
 cd "%~p0"
 
-copy /Y build\bin\debug\*.* \Build\win32\winD\
-copy /Y build\bin\release\*.* \Build\win32\winR\
+copy /Y build\bin\debug\clucene-contribs-libd.* \Build\win32\winD\
+copy /Y build\bin\release\clucene-contribs-lib.* \Build\win32\winR\
+copy /Y build\bin\debug\clucene-cored.* \Build\win32\winD\
+copy /Y build\bin\release\clucene-core.* \Build\win32\winR\
+copy /Y build\bin\debug\clucene-sharedd.* \Build\win32\winD\
+copy /Y build\bin\release\clucene-shared.* \Build\win32\winR\
+copy /Y build\bin\debug\cl_test.* \Build\win32\winD\
+copy /Y build\bin\release\cl_test.* \Build\win32\winR\
 @echo off
 
 PATH=%PATH%;"c:\Program Files (x86)\Microsoft Visual Studio 8\Common7\Tools\Bin\"
