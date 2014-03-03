87b86c07932539456376ab30d9c167af7614981a
borekk <kostka@tovek.cz>
2014-03-03 17:12:13 +0100

    merge fixes

--- _publish64.cmd
+++ _publish64.cmd
@@ -1,11 +1,11 @@
 %~d0
 cd "%~p0"
 
-copy /Y build64\bin\debug\clucene-contribs-libd.* \Build\win64\winD\
-copy /Y build64\bin\release\clucene-contribs-lib.* \Build\win64\winR\
-copy /Y build64\bin\debug\clucene-cored.* \Build\win64\winD\
-copy /Y build64\bin\release\clucene-core.* \Build\win64\winR\
-copy /Y build64\bin\debug\clucene-sharedd.* \Build\win64\winD\
-copy /Y build64\bin\release\clucene-shared.* \Build\win64\winR\
+copy /Y build64\bin\debug\clucene-contribs-libd.* \Build\x64\winD\
+copy /Y build64\bin\release\clucene-contribs-lib.* \Build\x64\winR\
+copy /Y build64\bin\debug\clucene-cored.* \Build\x64\winD\
+copy /Y build64\bin\release\clucene-core.* \Build\x64\winR\
+copy /Y build64\bin\debug\clucene-sharedd.* \Build\x64\winD\
+copy /Y build64\bin\release\clucene-shared.* \Build\x64\winR\
 
 pause
