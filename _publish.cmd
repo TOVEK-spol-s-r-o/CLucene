172aca25350a2c3a1380d9b25c3d690b911a960b
borekk <kostka@tovek.cz>
2016-08-03 13:50:37 +0200

    publishing scripts modified

--- _publish.cmd
+++ _publish.cmd
@@ -7,6 +7,21 @@ copy /Y build\bin\debug\clucene-cored.* \Build\win32\winD\
 copy /Y build\bin\release\clucene-core.* \Build\win32\winR\
 copy /Y build\bin\debug\clucene-sharedd.* \Build\win32\winD\
 copy /Y build\bin\release\clucene-shared.* \Build\win32\winR\
+
+copy /Y build\bin\debug\clucene-contribs-libd.* \devlibs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\release\clucene-contribs-lib.* \devlibs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\debug\clucene-cored.* \devlibs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\release\clucene-core.* \devlibs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\debug\clucene-sharedd.* \devlibs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\release\clucene-shared.* \devlibs2012\clucene_2_3_2_tvk_2\lib\win32
+
+copy /Y build\bin\debug\clucene-contribs-libd.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\release\clucene-contribs-lib.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\debug\clucene-cored.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\release\clucene-core.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\debug\clucene-sharedd.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\win32
+copy /Y build\bin\release\clucene-shared.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\win32
+
 @echo off
 
 pause
--- _publish64.cmd
+++ _publish64.cmd
@@ -7,6 +7,23 @@ copy /Y build64\bin\debug\clucene-cored.* \Build\x64\winD\
 copy /Y build64\bin\release\clucene-core.* \Build\x64\winR\
 copy /Y build64\bin\debug\clucene-sharedd.* \Build\x64\winD\
 copy /Y build64\bin\release\clucene-shared.* \Build\x64\winR\
+
+copy /Y build64\bin\debug\clucene-contribs-libd.* \devlibs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\release\clucene-contribs-lib.* \devlibs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\debug\clucene-cored.* \devlibs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\release\clucene-core.* \devlibs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\debug\clucene-sharedd.* \devlibs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\release\clucene-shared.* \devlibs2012\clucene_2_3_2_tvk_2\lib\x64
+
+copy /Y build64\bin\debug\clucene-contribs-libd.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\release\clucene-contribs-lib.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\debug\clucene-cored.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\release\clucene-core.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\debug\clucene-sharedd.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\x64
+copy /Y build64\bin\release\clucene-shared.* \\data\Development\devlibs\libs2012\clucene_2_3_2_tvk_2\lib\x64
+
+
+
 @echo off
 
 pause
